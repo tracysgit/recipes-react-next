872218e2594d4262538c21e6200534f32b45f641
tracysgit <tracy@wildiris.net>
2024-01-09 17:25:19 -0600

    Updated ESLint issues.

--- app/categories/[id]/page.tsx
+++ app/categories/[id]/page.tsx
@@ -5,14 +5,14 @@ import { notFound } from 'next/navigation';
 
 export default async function Recipe({ params }: { params: { id: string } }) {
   const id = params.id;
-  const [invoice, customers] = await Promise.all([
-    // fetchInvoiceById(id),
-    // fetchCustomers(),
-  ]);
+  // const [invoice, customers] = await Promise.all([
+  //   // fetchInvoiceById(id),
+  //   // fetchCustomers(),
+  // ]);
 
-  if (!invoice) {
-    notFound();
-  }
+  // if (!invoice) {
+  //   notFound();
+  // }
 
   return (
     <main>
